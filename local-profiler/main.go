f31418bd53577b33077570392611dc0ac41415f5
Tymon Tumialis <tymontumialis@gmail.com>
2026-08-19 12:33:18 +0200

    getting started with prometheus

--- local-profiler/main.go
+++ local-profiler/main.go
@@ -10,6 +10,8 @@ import (
 	"sync"
 	"syscall"
 
+	prometheusserver "local-profiler/prometheus-server"
+
 	"github.com/cilium/ebpf/rlimit"
 )
 
@@ -61,6 +63,7 @@ func runGoRoutines(obj modules.BPFObject, sync *modules.SyncStruct, errChan chan
 }
 
 func main() {
+	prometheusserver.ConnectToPrometheus()
 
 	errChan := make(chan error, NUMBER_OF_GO_ROUTINES)
 
--- local-profiler/prometheus-server/server.go
+++ local-profiler/prometheus-server/server.go
@@ -1,1 +1,16 @@
 package prometheusserver
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func ConnectToPrometheus() {
+	http.Handle("/metrics", promhttp.Handler())
+
+	// Uruchamiamy serwer HTTP na porcie 8080
+	log.Println("Serwer nasłuchuje na porcie 8080. Sprawdź http://localhost:8080/metrics")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
