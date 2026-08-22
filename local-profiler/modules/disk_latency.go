0eeb1c336ae34a851ded7a480b2bb65bf2f2fe18
Tymon Tumialis <tymontumialis@gmail.com>
2026-08-22 21:42:27 +0200

    mul and add for IOStats is working

--- local-profiler/modules/disk_latency.go
+++ local-profiler/modules/disk_latency.go
@@ -1,12 +1,7 @@
 package modules
 
 import (
-	"fmt"
 	synchronization "local-profiler/synchronization"
-	"log"
-	"time"
-
-	"github.com/cilium/ebpf"
 )
 
 type IOStats struct {
@@ -36,60 +31,18 @@ func (s IOStats) Mul(scalar uint64) IOStats {
 
 func (o BPFObject) GetDiskLatency(sync *synchronization.SyncStruct) error {
 	defer sync.Wg.Done()
-
-	zeroValues := make([]IOStats, o.NumCPUs)
-
-	outFile, err := OpenFile("IOReadWrite.log")
-	if err != nil {
-		log.Printf("Error opening disk profiler log file : %v\n", err)
-		return err
+	profiler := ProfilerStruct{
+		SamplePeriod:    1,
+		TimeInterval:    5,
+		FileName:        "IOReadWrite",
+		ProfilerMessage: "I/O profiler started. Collecting data...",
+		sync:            sync,
 	}
-	defer outFile.Close()
 
-	fmt.Println("I/O profiler started. Collecting data...")
-
-	ticker := time.NewTicker(5 * time.Second)
-	defer ticker.Stop()
-
-	for {
-
-		select {
-		case <-sync.Ctx.Done():
-			fmt.Println("Cleaning disk latency")
-			return nil
-
-		case t := <-ticker.C:
-			t = time.Now()
-			t.Format("2006-01-02 15:04:05")
-
-			outFile.WriteString("Data from the Kernel: ")
-			outFile.Write([]byte(t.String()))
-			outFile.WriteString("\n")
-			var pid uint32
-			var perCPUValues []IOStats
-
-			iter := o.Objs.ProcessIoStats.Iterate()
-			for iter.Next(&pid, &perCPUValues) {
-				userName := getUserForPID(pid)
-
-				var totalCount IOStats
-				for _, val := range perCPUValues {
-					totalCount.ReadBytes += val.ReadBytes
-					totalCount.ReadCount += val.ReadCount
-					totalCount.WriteBytes += val.WriteBytes
-					totalCount.WriteCount += val.WriteCount
-				}
-				if totalCount.ReadBytes > 0 || totalCount.WriteBytes > 0 {
-					myString := fmt.Sprintf("Data for %d pid, UserId - %s : \n Total ReadBytes : %d, Total ReadCount : %d, Total WriteBytes : %d, Total WriteCount %d\n", pid, userName, totalCount.ReadBytes, totalCount.ReadCount, totalCount.WriteBytes, totalCount.WriteCount)
-					outFile.WriteString(myString)
-				}
-				err := o.Objs.ProcessIoStats.Update(pid, zeroValues, ebpf.UpdateAny)
-				if err != nil {
-					log.Printf("Failed to reset key %d: %v", pid, err)
-					return err
-				}
-			}
-		}
-
+	profilerData := ProfilerData[IOStats]{
+		zeroValues: &[]IOStats{},
 	}
+
+	return RunGoRoutine(&profiler, o.Objs.ProcessIoStats, profilerData)
+
 }
