3dcd20e38d845c4630467cb124c8ed6a1162983a
Tymon Tumialis <tymontumialis@gmail.com>
2026-08-22 21:46:26 +0200

    mul and add for PageFaultStruct is working

--- local-profiler/modules/page_faults.go
+++ local-profiler/modules/page_faults.go
@@ -1,12 +1,7 @@
 package modules
 
 import (
-	"fmt"
 	synchronization "local-profiler/synchronization"
-	"log"
-	"time"
-
-	"github.com/cilium/ebpf"
 )
 
 type PageFaultStruct struct {
@@ -14,61 +9,36 @@ type PageFaultStruct struct {
 	MajorFaults uint64
 }
 
+func (p PageFaultStruct) Add(other PageFaultStruct) PageFaultStruct {
+	return PageFaultStruct{
+		MinorFaults: p.MinorFaults + other.MinorFaults,
+		MajorFaults: p.MajorFaults + other.MajorFaults,
+	}
+
+}
+
+func (p PageFaultStruct) Mul(scalar uint64) PageFaultStruct {
+	return PageFaultStruct{
+		MinorFaults: p.MinorFaults * scalar,
+		MajorFaults: p.MajorFaults * scalar,
+	}
+}
+
 func (o BPFObject) PageFaults(sync *synchronization.SyncStruct) error {
 	defer sync.Wg.Done()
 
-	outFile, err := OpenFile("pageFaults.log")
-	if err != nil {
-		log.Printf("Error opening a file : %v\n", err)
-		return err
+	profiler := ProfilerStruct{
+		SamplePeriod:    1,
+		TimeInterval:    5,
+		FileName:        "pageFaults",
+		ProfilerMessage: "RAM Profiler is working, listening for page faults...",
+		sync:            sync,
 	}
-	defer outFile.Close()
 
-	zeroValues := make([]PageFaultStruct, o.NumCPUs)
-
-	fmt.Println("RAM Profiler is working, listening for page faults...")
-
-	ticker := time.NewTicker(5 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-sync.Ctx.Done():
-			fmt.Println("closing page faults")
-			return nil
-
-		case t := <-ticker.C:
-			t = time.Now()
-			t.Format("2006-01-02 15:04:05")
-
-			outFile.WriteString("---Active processes (Top page faults)---")
-			outFile.Write([]byte(t.String()))
-			outFile.WriteString("\n")
-
-			var pid uint32
-			var perCpuCount []PageFaultStruct
-			var iter = o.Objs.PageFault.Iterate()
-
-			for iter.Next(&pid, &perCpuCount) {
-				userName := getUserForPID(pid)
-
-				var totalCount PageFaultStruct = PageFaultStruct{0, 0}
-				for _, coreCount := range perCpuCount {
-					totalCount.MajorFaults += coreCount.MajorFaults
-					totalCount.MinorFaults += coreCount.MinorFaults
-				}
-				if totalCount.MajorFaults > 0 || totalCount.MinorFaults > 0 {
-					myString := fmt.Sprintf("PID: %d , UserName : %s | Number of minor page faults: %d | Number of major page faults: %d \n", pid, userName, totalCount.MinorFaults, totalCount.MajorFaults)
-					outFile.WriteString(myString)
-				}
-
-				err := o.Objs.PageFault.Update(pid, zeroValues, ebpf.UpdateAny)
-				if err != nil {
-					log.Printf("Failed to reset key %d: %v", pid, err)
-					return err
-				}
-			}
-		}
-
+	profilerData := ProfilerData[PageFaultStruct]{
+		zeroValues: &[]PageFaultStruct{},
 	}
+
+	return RunGoRoutine(&profiler, o.Objs.PageFault, profilerData)
+
 }
